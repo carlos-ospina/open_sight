bb850ba025d5818ab72933d2347fbc44d04d6982
Carlos Jose Ospina <carlos.jose.ospina@gmail.com>
2023-10-16 20:36:27 +0200

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to Open Sight</h1>
+            <p>Discover a new perspective.</p>
+        </div>
+    );
+}
+
+export default HomePage;
